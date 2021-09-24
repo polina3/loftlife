d20a3832bcf51045d03002a29ca1534b4e70eeac
polina <supran955@gmail.com>
2021-09-24 20:33:33 +0300

    стилизация

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,3 +2,19 @@
 //= lib/bootstrap.bundle.min.js
 //= lib/inputmask.js
 //= lib/lazyload.js
+
+
+$(document).ready(function(){  
+    // прошлое положение страницы
+    var lastScrollTop = 0;
+    $(window).scroll(function(event){
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop){
+            $("header").addClass("hidden");
+        } else {
+            
+            $("header").removeClass("hidden");
+        }
+        lastScrollTop = st;
+    });
+})
