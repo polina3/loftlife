6dac87bc5a5d19f79199269962d0628e73cae36f
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2021-10-24 19:31:57 +0300

    crm

--- dev/js/admin.js
+++ dev/js/admin.js
@@ -9,4 +9,30 @@ $(document).ready(function(){
         $('.call-section').removeClass('active');
         $(section).addClass('active');
     })
+
+    $('.table-call__status').on('change', function(){
+        var idRow = $(this).attr('data-id');
+        var newStatus = $(this).val();
+        console.log(newStatus);     
+        $.ajax({
+            url: '/modules/admin/controller/ajax/leadEditingStatus.php',        
+            method: 'post',                     
+            data: {id: idRow, status: newStatus},     
+            success: function(data){   
+                location.reload();         
+            }
+        });
+    })
+
+    $('.table-call__del').on('click', function(){
+        var idRow = $(this).attr('data-id');  
+        $.ajax({
+            url: '/modules/admin/controller/ajax/leadDel.php',        
+            method: 'post',                     
+            data: {id: idRow},     
+            success: function(data){   
+                location.reload();         
+            }
+        });
+    })
 })
