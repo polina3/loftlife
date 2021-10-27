73bd8437ce2eb7556ff7287b62301fea6c2729c1
polina <supran955@gmail.com>
2021-10-27 18:56:29 +0300

    admin av

--- dev/js/admin.js
+++ dev/js/admin.js
@@ -4,6 +4,21 @@
 
 
 $(document).ready(function(){  
+
+    $('.js-validate').submit(function (e) {
+        var form = $(this);
+        form.find('*[data-validate]').each(function (i, elem) {
+            var value = $(elem).val();
+            if ((!value || value === undefined || value === null || value === '')) {
+                $(elem).addClass('is-invalid');
+                setTimeout(function () {
+                    $(elem).removeClass('is-invalid')
+                }.bind(elem), 2000);
+                e.preventDefault();
+            }
+        })
+    })
+
     $('.nav-call-item').on('click', function(){
         var section = '.'+$(this).attr('data-section');
         $('.call-section').removeClass('active');
@@ -35,4 +50,18 @@ $(document).ready(function(){
             }
         });
     })
+
+    $('.btn-av').on('click', function(){  
+        $.ajax({
+            url: '/modules/admin/controller/ajax/leadDel.php',        
+            method: 'post',                     
+            data: {id: idRow},     
+            success: function(data){   
+                location.reload();         
+            }
+        });
+    })
+
+
+    
 })
